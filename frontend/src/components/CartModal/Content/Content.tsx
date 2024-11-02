a19adce5d929afbaf5483d37fbec1ed3ad398572
12kot <art.artem.nik@gmail.com>
2024-11-03 02:30:37 +0300

    ifx

--- frontend/src/components/CartModal/Content/Content.tsx
+++ frontend/src/components/CartModal/Content/Content.tsx
@@ -89,22 +89,21 @@ const Card = ({ setIsOpen, id, title, price, image }: CardProps) => {
 
   const dispatch = useAppDispatch();
 
-  const handleAddToCart = (e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>) => {
-    e.preventDefault();
+  const handleAddToCart = (e: MouseEvent<HTMLDivElement, globalThis.MouseEvent>) => {
     e.stopPropagation();
     dispatch(addItemToCart({ id }));
   };
 
   return (
-    <Button buttonType="default" className={styles.card} onClick={() => handleNavigate(`${APP_ROUTES.PRODUCT}/${1}`)}>
+    <Button buttonType="default" className={styles.card} onClick={() => handleNavigate(`${APP_ROUTES.PRODUCT}/${id}`)}>
       <img loading="lazy" className={styles.card_img} src={image} />
       <div className={styles.card_name}>
         <p>{title}</p>
         <div className={styles.card_name__price}>
           <b>{t('common:product.price', { price })}</b>
-          <Button buttonType="default" className={styles.delete} onClick={(e) => handleAddToCart(e)}>
+          <div tabIndex={0} className={styles.delete} onClick={(e) => handleAddToCart(e)}>
             <SVGDelete />
-          </Button>
+          </div>
         </div>
       </div>
     </Button>
